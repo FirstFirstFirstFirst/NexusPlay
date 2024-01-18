95c6b3317e3e1c9be9632ee37a7f07426687dad9
Cartooninw <radthapoomrodnin@gmail.com>
2024-01-18 23:08:32 +0700

    StorePage Upadate

---
+++ components/StorePage/ForGuest/Overview.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <div>
+      <button>Overview</button>
+      <button>Vitories</button>
+    </div>
+  )
+}
+
+export default Overview
--- components/StorePage/ForGuest/Top.tsx
+++ components/StorePage/ForGuest/Top.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Overview from './Overview'
 const Top = () => {
   return (
-    <div>
+    <div className='flex flex-row justify-between w-[inherit]'>
       <p className='text-3xl font-bold'>Game Name</p>
+      <Overview />
     </div>
   )
 }
