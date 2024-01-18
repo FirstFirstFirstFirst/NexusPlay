e5154733972662508bbad711f0f0e7b39527290e
Cartooninw <radthapoomrodnin@gmail.com>
2024-01-18 22:39:03 +0700

    + Set background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className}  bg-gradient-to-l from-black to-[#B00101]` }>
         <Navbar />
         {children}
       </body>
