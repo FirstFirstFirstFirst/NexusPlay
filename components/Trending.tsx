ef5a5487df046ed8aec3de9ddc4f612f91b754e7
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2024-01-18 22:45:22 +0700

    complete trending component

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -4,21 +4,30 @@ import React from "react";
 
 const Trending = () => {
   return (
-    <div>
-      <p className="text-6xl">Trending</p>
-      <div className="grid grid-cols-3 gap-4">
+    <div className="flex flex-col gap-12">
+      <p className="text-6xl text-white">Trending</p>
+      <div className="grid grid-cols-3 gap-12">
         {games.map((game) => (
-          <div key={game.id} className="flex flex-col items-center">
-            <Image
-              src={game.photo}
-              alt={game.name}
-              width={100}
-              height={100}
-              className="max-w-full h-auto mb-2"
-            />
-            <h3 className="text-lg font-bold">{game.name}</h3>
-            <p className="text-sm">{game.description}</p>
-            <p className="text-sm">Price: ${game.price}</p>
+          <div
+            key={game.id}
+            className="flex flex-col items-center xl:w-fit h-auto rounded-xl bg-white"
+          >
+            <div className="relative">
+              <Image
+                src={game.photo}
+                alt={game.name}
+                width={500}
+                height={570}
+                className="object-cover h-[570px] w-full rounded-t-xl"
+              />
+            </div>
+            <div className="flex flex-col p-6 bg-white rounded-b-xl w-full gap-6">
+              <div className="flex flex-row justify-between gap-4">
+                <h3 className="text-3xl font-bold w-2/3">{game.name}</h3>
+                <p className="text-3xl w-1/3 text-end">${` ${game.price}`}</p>
+              </div>
+              <p className="text-lg">{game.description}</p>
+            </div>
           </div>
         ))}
       </div>
