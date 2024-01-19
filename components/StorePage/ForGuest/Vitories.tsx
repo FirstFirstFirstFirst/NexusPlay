1ecf88a14978173303f915517d81db9ab895268e
Cartooninw <radthapoomrodnin@gmail.com>
2024-01-19 15:56:50 +0700

    StorePage 90% (Waiting for API) (90% design account UI remaining)

--- components/StorePage/ForGuest/Vitories.tsx
+++ components/StorePage/ForGuest/Vitories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import Card from './Card'
-
+import Preveiw from './Preveiw'
 type VitoriesProps = {
   Cardcount: number;
 
@@ -11,28 +11,7 @@ const Vitories:React.FC<VitoriesProps> = ({Cardcount}) => {
   const cardsArray = Array.from({ length: Cardcount }, (_, index) => index);
   return (
     <div>
-      <Image src='https://images.unsplash.com/photo-1511882150382-421056c89033?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            alt='Wait for api'
-            width={1200}
-            height={100} />
-      <div className='flex flex-row gap-3 justify-center mt-2'>
-      <Image src='https://images.unsplash.com/photo-1511882150382-421056c89033?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            alt='Wait for api'
-            width={70}
-            height={40} />
-      <Image src='https://images.unsplash.com/photo-1511882150382-421056c89033?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            alt='Wait for api'
-            width={70}
-            height={40} />
-      <Image src='https://images.unsplash.com/photo-1511882150382-421056c89033?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            alt='Wait for api'
-            width={70}
-            height={40} />
-      <Image src='https://images.unsplash.com/photo-1511882150382-421056c89033?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            alt='Wait for api'
-            width={70}
-            height={40} />
-      </div>
+      <Preveiw />
       <div className='grid grid-cols-3 gap-[10px]'>
       {cardsArray.map((_, index) => <Card key={index} />)}
       </div>
