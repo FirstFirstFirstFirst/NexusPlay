9769c3fd1577657998d7bcd92808f152c39762c5
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2024-01-21 14:39:41 +0700

    type fixed

--- components/Navbar/Login.tsx
+++ components/Navbar/Login.tsx
@@ -8,7 +8,7 @@ const Login = () => {
     <div
       className="flex items-center justify-center border border-black rounded-lg uppercase bg-[#B30000] cursor-pointer transition-all duration-300 hover:bg-[#9c0000] py-3 px-5 h-12"
       onClick={() => {
-        router.push(`/LoginPage`);
+        router.push(`/loginPage`);
       }}
     >
       Login
